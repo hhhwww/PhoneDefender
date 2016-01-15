451fbd3f5dab67fd54ac8d33abde0cd6b7c757aa
He Wei <xdhhhwww@foxmail.com>
2016-01-15 11:56:09 +0800

    BaseSetupActivity中添加加入SharedPreference的逻辑

--- PhoneDefender/app/src/main/java/com/xd/phonedefender/hw/activity/BaseSetupActivity.java
+++ PhoneDefender/app/src/main/java/com/xd/phonedefender/hw/activity/BaseSetupActivity.java
@@ -1,5 +1,6 @@
 package com.xd.phonedefender.hw.activity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.GestureDetector;
@@ -13,14 +14,22 @@ import android.view.View;
 public abstract class BaseSetupActivity extends AppCompatActivity {
 
     protected GestureDetector gestureDetector;
+    protected SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        sp = getSharedPreferences("config", MODE_PRIVATE);
+
+
         gestureDetector = new GestureDetector(new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                if (Math.abs(e2.getRawY() - e1.getRawX()) > 100)
+                    return true;
+                if (Math.abs(velocityX) < 100 || Math.abs(velocityY) < 100)
+                    return true;
                 if (e2.getRawX() - e1.getRawX() > 200)
                     showPreviousPage();
                 if (e1.getRawX() - e2.getRawX() > 200)
--- PhoneDefender/app/src/main/java/com/xd/phonedefender/hw/activity/Setup4Activity.java
+++ PhoneDefender/app/src/main/java/com/xd/phonedefender/hw/activity/Setup4Activity.java
@@ -1,7 +1,6 @@
 package com.xd.phonedefender.hw.activity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.xd.phonedefender.R;
@@ -11,18 +10,11 @@ import com.xd.phonedefender.R;
  */
 public class Setup4Activity extends BaseSetupActivity {
 
-    private SharedPreferences sp;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup4);
 
-        initViews();
-    }
-
-    private void initViews() {
-        sp = getSharedPreferences("config", MODE_PRIVATE);
     }
 
     @Override
